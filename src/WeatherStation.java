f3d90f200bf89bec50214a793ddb215b11a6ba55
Gundeep <Gundeep@DESKTOP-L10JNPV>
2019-07-19 13:57:58 -0400

    Observable commit

--- src/WeatherStation.java
+++ src/WeatherStation.java
@@ -1,14 +1,16 @@
+import java.util.Observable;
 
-public class WeatherStation {
+//Observable is publisher
+public class WeatherStation extends Observable {
 	private int temperature;
 
 	public WeatherStation(int temp) {
 		this.temperature = temp;
 	}
-	
+
 	public void sayHello() {
-		System.out.println("Hello I'm a weatherStation : "+this.temperature);
-		
+		System.out.println("Hello I'm a weatherStation : " + this.temperature);
+
 	}
 
 	public int getTemperature() {
@@ -16,7 +18,13 @@ public class WeatherStation {
 	}
 
 	public void setTemperature(int temperature) {
+		//1. Chage the temp
 		this.temperature = temperature;
+		//2. Tell everyone about the weather
+		//Indicate something changed
+		this.setChanged(); // methods of observable
+		// Tell subscirbers about the new temperature
+		this.notifyObservers(this.temperature); // methods of observable
 	}
 
 }
